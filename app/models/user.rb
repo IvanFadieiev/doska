8e56fa3a3ba24fc3d3ef6914a891a0e8231a7019
Ivan <ivanfadeev91@gmail.com>
2015-10-12 18:38:06 +0300

    sign_in/out_finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,8 @@ class User < ActiveRecord::Base
   self.city = city.capitalize
   self.country = country.capitalize
   end
-
+  before_create :create_remember_token
+  
   validates :user, presence: true, length: { maximum: 50 }
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -26,4 +27,19 @@ class User < ActiveRecord::Base
   validates :city, presence: true
   validates :country, presence: true
   validates_attachment :avatar, content_type: { content_type: /\Aimage\/.*\Z/ }
+
+def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151011193234) do
+ActiveRecord::Schema.define(version: 20151012134246) do
 
   create_table "users", force: :cascade do |t|
     t.string   "user"
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 20151011193234) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
