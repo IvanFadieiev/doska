1a706b772419b1d5e5bbdecc853458b2ec28960d
IvanFadieiev <ivanfadeev91@gmail.com>
2015-12-11 01:08:53 +0200

    herokku

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module Doska
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
- 
     config.generators do |g|
         g.template_engine :haml
         g.test_framework   :rspec, fixtures: true, views:false
